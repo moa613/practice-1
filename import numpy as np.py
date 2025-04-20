eb2a0d95eb1e8e77eb29326418dfb00a873374bb
moa613 <moa@sookmyung.ac.kr>
2025-04-21 01:32:36 +0900

    처음 파일 업로드

---
+++ import numpy as np.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+a=np.array([0,1])
+b=np.array([-1,np.sqrt(3)])
+
+a_dot_b=np.dot(a,b)
+a_mul_b=np.linalg.norm(a) *np.linalg.norm(b)
+cos_theta=a_dot_b/a_mul_b
+theta=np/arccos(cos_theta)
+theta_degree=theta/np.pi*180
+
+print(a_dot_b)
+print(a_mul_b)
+print(cos_theta)
+print(theta)
+print(theta_degree)
